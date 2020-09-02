f0c59b17864bd097e9ea6ab206e8810c9c875d1f
BrianCambron <69121396+BrianCambron@users.noreply.github.com>
2020-09-02 14:32:47 -0400

    dynamically modify the length of the timer bar

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
   function checkTime(i) {
-  if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+  if (i < 10) {i = "0" + i};  // <- add zero in front of numbers < 10
   return i;
 }
+
+const progressBar = document.querySelector('.clock-progress-bar') //<- better to use queryselector rather than getElementbyId
+
 setInterval(function() {
   let d = new Date();
   console.log(d);
@@ -17,10 +20,8 @@ setInterval(function() {
 
   let currenttime = `${hours}:${minutes}:${seconds}`;
   console.log(currenttime);
+  console.log(seconds / 60); //<-percentage of a minute that the current seconds represents
+  progressBar.style.width = `${seconds / 60 * 14}rem` //<- Using the percent above, dynamically modify the length of the timer bar
 
-  function calculatePercent(percent, num){
-    return (percent / 100) * num;
-}
-console.log(calculatePercent(d.getMinutes(), d.getSeconds()));
 document.getElementById("clock-display").innerHTML = currenttime;
 console.log(currenttime);} , 1000);
